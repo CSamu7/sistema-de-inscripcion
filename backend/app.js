d5ec792fda5652adc5f7cc29be3873fb51c92f95
CSamu7 <samuel.pdg@hotmail.com>
2023-05-17 00:42:40 -0600

    chore: modify userRouter

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-const userRouter = require("../backend/routes/user.routes.js");
+const usuarioRouter = require("../backend/routes/user.routes.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/", userRouter);
+app.use("/api/v1/user/", usuarioRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Aplicación escuchando");

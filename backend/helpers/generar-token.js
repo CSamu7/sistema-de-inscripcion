0c6ba9f425d2e4176dd4838540455b39eb26cf29
CSamu7 <samuel.pdg@hotmail.com>
2023-05-17 00:40:33 -0600

    feat: add token to login

---
+++ backend/helpers/generar-token.js
@@ -0,0 +1,16 @@
+const { SignJWT } = require("jose");
+
+const generarToken = async (payload) => {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const alg = "HS256";
+
+  const jwt = await new SignJWT({ payload })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime("1hr")
+    .sign(secret);
+
+  return jwt;
+};
+
+module.exports = generarToken;

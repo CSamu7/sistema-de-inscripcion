cbc8941e3b8f69af65500f3cb73c5c6fd7854e63
CSamu7 <samuel.pdg@hotmail.com>
2023-05-17 00:39:30 -0600

    chore: add user model.

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,5 +1,34 @@
-class User {
-  constructor() {}
+const BaseDeDatos = require("./db");
 
-  getUser() {}
+class Usuario {
+  numeroDeCuenta;
+  contrasenia;
+
+  constructor(numeroDeCuenta, contrasenia) {
+    this.numeroDeCuenta = Number(numeroDeCuenta);
+    this.contrasenia = contrasenia;
+  }
+
+  conseguirNumeroDeCuenta() {
+    return this.numeroDeCuenta;
+  }
+
+  conseguirContrasenia() {
+    return this.contrasenia;
+  }
+
+  async consultarUsuario() {
+    const conexion = await BaseDeDatos.conectar();
+
+    const query = `SELECT numero_de_cuenta, cast(aes_decrypt(contrasenia, ?) as char) as contrasenia, nombre, apellido_paterno, apellido_materno, es_admin FROM usuario WHERE numero_de_cuenta = ?`;
+
+    const [rows] = await conexion.execute(query, [
+      process.env.DATABASE_SECRET_KEY,
+      this.numeroDeCuenta,
+    ]);
+
+    return rows;
+  }
 }
+
+module.exports = Usuario;

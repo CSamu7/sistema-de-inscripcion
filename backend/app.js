14a377b154fa590948245b4698d826b4cd43a513
CSamu7 <samuel.pdg@hotmail.com>
2023-05-19 02:25:16 -0600

    feat: add group router to app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const usuarioRouter = require('../backend/routes/user.routes.js');
+const grupoRouter = require('./routes/group.routes.js');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/user/', usuarioRouter);
+app.use('/api/v1/grupo/', grupoRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Aplicación escuchando');

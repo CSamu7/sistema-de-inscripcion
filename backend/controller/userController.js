5984002c6847f0df536624686b94e2f7f1451e79
CSamu7 <samuel.pdg@hotmail.com>
2023-05-19 02:24:58 -0600

    feat: add consultarUsuario,

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,10 +1,32 @@
 const generarToken = require('../helpers/generar-token');
 const validarUsuario = require('../helpers/validar-usuario');
 const Usuario = require('../model/user.model');
+const validarTokenJWT = require('../helpers/validar-token-jwt');
 
 const controladorUsuario = {};
 
-controladorUsuario.consultarUsuario = async (req, res) => {};
+controladorUsuario.consultarUsuario = async (req, res) => {
+  try {
+    const token = req.get('authorization');
+    //FIXME: FALTA VERIFICAR AQUI
+    const payload = await validarTokenJWT(token);
+
+    const usuario = new Usuario(payload.payload);
+
+    const datosDeUsuario = await usuario.consultarUsuario();
+
+    delete datosDeUsuario[0].contra;
+
+    res.status(200).json(datosDeUsuario);
+  } catch (error) {
+    //FIXME: Poner mensajes en español
+    return res.status(403).json({
+      status: 403,
+      description: error.message,
+      error: true
+    });
+  }
+};
 
 controladorUsuario.autenticarUsuario = async (req, res) => {
   const { numeroDeCuenta, contra } = req.body;

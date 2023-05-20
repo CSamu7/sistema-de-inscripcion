55b5e52342c1cb38a33b8faff0178f55079a0caf
CSamu7 <samuel.pdg@hotmail.com>
2023-05-20 00:01:33 -0600

    feat: add endpoints to group

--- backend/controller/groupController.js
+++ backend/controller/groupController.js
@@ -1,13 +1,38 @@
-const groupController = {};
+const validarTokenJWT = require('../helpers/validar-token-jwt');
+const Grupo = require('../model/group.model');
 
-groupController.getGroup = (req, res) => {
-  res.send("Hola");
+const grupoController = {};
+
+grupoController.consultarGrupo = async (req, res) => {
+  try {
+    const idGrupo = req.params.idGrupo;
+    const token = req.get('authorization');
+
+    await validarTokenJWT(token);
+
+    const grupo = await new Grupo(idGrupo).consultarGrupo();
+
+    res.status(200).json(grupo);
+  } catch (error) {
+    //TODO: Manejar errores.
+    console.log(error);
+  }
 };
 
-groupController.getAllGroups = (req, res) => {};
+grupoController.consultarTodosLosGrupos = async (req, res) => {
+  try {
+    const token = req.get('authorization');
 
-groupController.postGroup = (req, res) => {};
+    await validarTokenJWT(token);
 
-groupController.patchGroup = (req, res) => {};
+    const grupo = await new Grupo().consultarTodosLosGrupos();
 
-module.exports = groupController;
+    res.status(200).json(grupo);
+  } catch (error) {
+    //TODO: Manejar errores
+  }
+};
+
+grupoController.modificarGrupo = (req, res) => {};
+
+module.exports = grupoController;

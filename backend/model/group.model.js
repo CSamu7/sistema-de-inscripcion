79810ecf1025a571b11425fb8ce8e7781ff60bc1
CSamu7 <89798373+CSamu7@users.noreply.github.com>
2023-05-23 02:19:14 -0600

    Enroll frontend (#7)
    * style: change variables to spanish
    
    * feat: add route to group
    
    * refactor: add function to validate token
    
    * feat: add consultarUsuario,
    
    * feat: add group router to app
    
    * fix: add comma to sql
    
    * feat: add endpoints to group
    
    * feat: add enroll page
    
    Co-authored-by: SebasRodzSanz <SebasRodzSanz@users.noreply.github.com>
    
    * refactor: remove bootstrap from project
    
    * style: change name app to inicio-de-sesion
    
    * chore: add cypress to project
    
    ---------
    
    Co-authored-by: SebasRodzSanz <SebasRodzSanz@users.noreply.github.com>

--- backend/model/group.model.js
+++ backend/model/group.model.js
@@ -1,5 +1,40 @@
-class Group {
-  constructor() {}
+const BaseDeDatos = require('./db');
 
-  getGroup() {}
+class Grupo {
+  idGrupo;
+
+  constructor(idGrupo) {
+    this.idGrupo = idGrupo;
+  }
+
+  async consultarGrupo() {
+    const query = `SELECT * FROM grupo WHERE id_grupo = ?`;
+
+    const connection = await BaseDeDatos.conectar();
+
+    const [rows] = await connection.execute(query, [this.idGrupo]);
+
+    if (rows.length === 0)
+      return [
+        {
+          idGrupo: 0
+        }
+      ];
+
+    return rows;
+  }
+
+  async consultarTodosLosGrupos() {
+    const query = `SELECT * FROM grupo`;
+
+    const connection = await BaseDeDatos.conectar();
+
+    const [rows] = await connection.execute(query);
+
+    return rows;
+  }
+
+  async modificarGrupo() {}
 }
+
+module.exports = Grupo;

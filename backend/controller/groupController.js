983fd82e5a67103123920c18e6dd7b3c9dd74849
CSamu7 <samuel.pdg@hotmail.com>
2023-05-24 02:08:31 -0600

    fix: remove unused function consultarGrupo

--- backend/controller/groupController.js
+++ backend/controller/groupController.js
@@ -3,23 +3,6 @@ const Grupo = require('../model/group.model');
 
 const grupoController = {};
 
-grupoController.consultarGrupo = async (req, res) => {
-  try {
-    const idGrupo = req.params.idGrupo;
-    const token = req.get('authorization');
-
-    await validarTokenJWT(token);
-
-    const grupo = await new Grupo(idGrupo).consultarGrupo();
-
-    res.status(200).json(grupo);
-  } catch (error) {
-    //TODO: Manejar errores.
-
-    console.log(error);
-  }
-};
-
 grupoController.consultarTodosLosGrupos = async (req, res) => {
   try {
     const token = req.get('authorization');
--- backend/model/group.model.js
+++ backend/model/group.model.js
@@ -7,23 +7,6 @@ class Grupo {
     this.idGrupo = idGrupo;
   }
 
-  async consultarGrupo() {
-    const query = `SELECT * FROM grupo WHERE id_grupo = ?`;
-
-    const connection = await BaseDeDatos.conectar();
-
-    const [rows] = await connection.execute(query, [this.idGrupo]);
-
-    if (rows.length === 0)
-      return [
-        {
-          idGrupo: 0
-        }
-      ];
-
-    return rows;
-  }
-
   async consultarTodosLosGrupos() {
     const query = `SELECT * FROM grupo`;
 
@@ -33,8 +16,6 @@ class Grupo {
 
     return rows;
   }
-
-  async modificarGrupo() {}
 }
 
 module.exports = Grupo;
